5218866f14ce22e2c434321623c883411cbc660e
Shohei Nagamura <shohei.nagamura@gmail.com>
2024-05-12 03:32:27 +0900

    ヘッダーのルーティング機能を実装

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,19 +4,22 @@ import { FC, memo } from "react";
 type Props = {
     onClose:() => void,
     isOpen: boolean;
+    onClickHome:() => void;
+    onClickSetting:() => void;
+    onClickUserManagement:() => void;
 }
 
 export const MenuDrawer:FC<Props> = memo (( props ) => {
-    const {onClose , isOpen} = props;
+    const {onClose , isOpen ,onClickHome, onClickSetting,onClickUserManagement} = props;
 
     return (
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay>
             <DrawerContent>
                 <DrawerBody p="0" bg="gray.100">
-                    <Button w="100%">トップ</Button>
-                    <Button w="100%">ユーザー一覧</Button>
-                    <Button w="100%">設定</Button>
+                    <Button w="100%" onClick={onClickHome} >トップ</Button>
+                    <Button w="100%" onClick={onClickUserManagement} >ユーザー一覧</Button>
+                    <Button w="100%" onClick={onClickSetting} >設定</Button>
                 </DrawerBody>
             </DrawerContent>
         </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -2,26 +2,45 @@ import { Box,Flex, Heading, Icon, Link, useDisclosure } from "@chakra-ui/react";
 import { FC, memo } from "react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { useNavigate } from "react-router-dom";
 
 export const Header:FC = memo (() => {
     const { isOpen , onOpen , onClose} = useDisclosure();
+    const navigate = useNavigate()
+
+    const navigateAndCloseDrawer = (path: string) => {
+        navigate(path);
+        onClose(); // 閉じる処理を追加
+    };
+
+    const onClickHome = () => {
+        navigateAndCloseDrawer('/home');
+    };
+
+    const onClickSetting = () => {
+        navigateAndCloseDrawer('/home/setting');
+    };
+
+    const onClickUserManagement = () => {
+        navigateAndCloseDrawer('/home/userManagement');
+    };
 
     return (
         <>
         <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{base:3 , md: 5}}>
-            <Flex align="center" as="a" mr={8} _hover={{ cursor:"pointer"}}>
+            <Flex align="center" as="a" mr={8} _hover={{ cursor:"pointer"}} onClick={onClickHome}>
                 <Heading as="h1" fontSize={{ base:"md", md:"lg" }}>ユーザー管理アプリ</Heading>
             </Flex>
             <Flex align="center" fontSize="sm" flexGrow={2} display={{base:"none" , md: "flex"}}>
-                <Box pr={4}>
+                <Box pr={4} onClick={onClickUserManagement}>
                     <Link>ユーザー一覧</Link>
                 </Box>
-                <Link>設定</Link>
+                <Link onClick={onClickSetting}>設定</Link>
                 <Icon></Icon>
             </Flex>
             <MenuIconButton onOpen={onOpen}/>
         </Flex>
-        <MenuDrawer isOpen={isOpen} onClose={onClose}/>
+        <MenuDrawer isOpen={isOpen} onClose={onClose} onClickHome={onClickHome} onClickSetting={onClickSetting} onClickUserManagement={onClickUserManagement}/>
         </>
     )
 } ) 
