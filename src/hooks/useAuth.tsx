877a26c089331d1b4a897a9ac4e231788bdca3ca
Shohei Nagamura <shohei.nagamura@gmail.com>
2024-05-12 18:02:05 +0900

    メッセージ表示機能の実装

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,24 +2,27 @@ import axios from "axios";
 import { useCallback, useState } from "react"
 import { User } from "../types/api/user";
 import { useNavigate } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 //ログイン機能
 export const useAuth = () => {
     const [loading , setLoading] = useState(false);
     const navigate = useNavigate()
+    const { showMessage } = useMessage();
 
     const login = useCallback((id:string) => {
         setLoading(true);
 
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
             if(res.data) {
+                showMessage({title:"ログインしました", status:'success'})
                 navigate('/home');
             } else {
-                alert("ユーザーが存在しません")
+                showMessage({title:"ユーザーが存在しません", status:'error'})
             }
         }).catch(() => 
-            alert("ログインできません")
+            showMessage({title:"ログインできません", status:'error'})
         ).finally(() => setLoading(false))
-    }, [navigate]);
+    }, [navigate, showMessage]);
     return {login , loading}
 }
